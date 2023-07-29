3c983a19f3692e8ced8f42909eda9af3a497ab51
AbdalrhmanAmmar <bodyty2n@gmail.com>
2023-07-29 22:19:29 +0200

    Notfound

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import Layout from "./../src/Components/Layout/Layout";
 import Home from "./../src/Components/Home/Home";
+import NotFound from "./../src/Components/NotFound/NotFound";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 function App() {
   let router = createBrowserRouter([
     {
       path: "",
       element: <Layout />,
-      children: [{ index: true, element: <Home /> }],
+      children: [
+        { index: true, element: <Home /> },
+        { path: "*", element: <NotFound /> },
+      ],
     },
   ]);
   return (
     <div>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -1,6 +1,23 @@
+import { Link } from "react-router-dom";
 import "./NotFound.module.css";
+import Notfound from "/images/error.svg";
 function NotFound() {
-  return <>NotFound</>;
+  return (
+    <>
+      <div className="mt-5 d-flex justify-content-center">
+        <img className="w-75" src={Notfound} alt="" />
+      </div>
+      <div className="d-flex justify-content-center">
+        <Link
+          to="/"
+          className="btn btn-success"
+          style={{ background: "#0aad0a" }}
+        >
+          Back To Home
+        </Link>
+      </div>
+    </>
+  );
 }
 
 export default NotFound;
