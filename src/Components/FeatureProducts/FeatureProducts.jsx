b59ad6b771f5e74f43799bde04f0cabe563cddd6
AbdalrhmanAmmar <bodyty2n@gmail.com>
2023-08-01 18:40:15 +0200

    product

--- src/Components/FeatureProducts/FeatureProducts.jsx
+++ src/Components/FeatureProducts/FeatureProducts.jsx
@@ -19,8 +19,20 @@ function FeatureProducts() {
       <div className="row">
         {products.map((product) => (
           <div key={product.id} className="col-md-2">
-            <div className="product">
-              <h3 className="h6">{product.title.split(' ').slice(0, 2)}</h3>
+            <div className="product px-2 py-4">
+              <img className="w-100" src={product.imageCover} alt="" />
+              <span className="categoryname">{product.category.name}</span>
+              <h3 className="h6 fw-bolder">
+                {product.title.split(" ").slice(0, 2).join(" ")}
+              </h3>
+              <div className="d-flex justify-content-between">
+                <span className="text-muted">{product.price} EGP</span>
+                <span>
+                  <i className="fas fa-star rating-color"></i>
+                  {product.ratingsAverage}
+                </span>
+              </div>
+              <button className="btn text-white w-100">Add</button>
             </div>
           </div>
         ))}
