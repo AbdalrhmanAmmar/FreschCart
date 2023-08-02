89f38f3b8a8b6f1db0fad784fa60ba767120bcff
AbdalrhmanAmmar <bodyty2n@gmail.com>
2023-08-02 21:10:25 +0200

    mapping

--- src/Components/ProductDetails/ProductDetails.jsx
+++ src/Components/ProductDetails/ProductDetails.jsx
@@ -2,8 +2,19 @@ import { useParams } from "react-router-dom";
 import "./ProductDetails.module.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function ProductDetails() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   const [isloading, setisloading] = useState(false);
   const [ProductDetails, setProductDetails] = useState([]);
   let params = useParams();
@@ -31,8 +42,14 @@ function ProductDetails() {
         <>
           <div className="row py-3 align-items-center">
             <div className="col-md-4">
-              <img className="w-100" src={ProductDetails.imageCover} alt="" />
+              <Slider {...settings}>
+                {/* {ProductDetails?.images.map((imag) => (
+                  <img src={imag} />
+                ))} */}
+                <img src={ProductDetails?.imageCover} />
+              </Slider>
             </div>
+
             <div className="col-md-8">
               <h3>{ProductDetails?.title}</h3>
               <p className="text-muted">{ProductDetails?.description}</p>
